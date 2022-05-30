51bce62f6292fb39c04bd2b5b64e4f11e7e40041
Malory_couvet <m.couvet@net.estia.fr>
2022-05-30 10:14:33 +0200

    yes ca marche

--- merchex/listings/.ipynb_checkpoints/models-checkpoint.py
+++ merchex/listings/.ipynb_checkpoints/models-checkpoint.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
-    name = models.CharField(max_length=100)
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+    
+    name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(max_length=50)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2022)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
     
 class Title(models.Model):
     name = models.CharField(max_length=100)
